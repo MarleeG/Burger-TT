a209a7d609242c45567942c1dc2aad7d2fb9ccb8
Christina Gerard <ChristinaGerard@Christinas-MBP.hsd1.ga.comcast.net>
2020-03-04 20:40:59 -0500

    created project skeleton

---
+++ server.js
@@ -0,0 +1,14 @@
+const log = console.log;
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, err => {
+    err ? log(err) : log(`http://localhost:${PORT}`);
+
+});
+
+
