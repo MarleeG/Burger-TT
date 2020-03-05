0a8085bcf3c17626372e698fb57bc38fd6b45ac1
Christina Gerard <ChristinaGerard@Christinas-MBP.hsd1.ga.comcast.net>
2020-03-04 21:42:47 -0500

    added schema content and inserted seed data

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS burgersTT_db;
+
+use burgersTT_db;
+
+CREATE TABLE burgers (
+    id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    burger_name varchar(255) NOT NULL, 
+    devoured BOOL DEFAULT false
+)
