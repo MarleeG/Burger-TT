7d97af69f1c16190c87986b118ac4f59f8a0de4f
Christina Gerard <ChristinaGerard@Christinas-MBP.hsd1.ga.comcast.net>
2020-03-15 11:03:28 -0400

    included mysql remote connection on connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,22 @@
 const log = console.log;
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
-    database: 'burgersTT_db'
-});
+var connection;
+
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
+        database: 'burgersTT_db'
+    });
+}
+
+
 
 connection.connect(function (err) {
     // err ? log(err) : log("connected")
